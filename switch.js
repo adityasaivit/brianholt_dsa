4944ee32cbcaf4a6f08b262cf60a4c877f6e6192
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-08-17 17:40:37 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,23 @@
+let month=5
+
+switch(month)
+{
+	case 1:
+		console.log("january")
+		break
+
+	case 2:
+		consoe.log('february')
+		break;
+	case 3:
+		console.log('march')
+		break
+	case 4:
+		console.log('april')
+		break
+	case 5:
+		console.log('may')
+		break
+	default:
+		console.log("not defined") 
+}
